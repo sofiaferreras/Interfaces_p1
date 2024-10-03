652139f91fa686c17bc92173d83ff3cfdecb6ec0
sofiaferreras <100498789@alumnos.uc3m.es>
2024-10-03 08:48:00 +0200

    primeros cambios JavaScript

---
+++ script/script_1.js
@@ -0,0 +1,20 @@
+const fechaObjetivo = new Date("Dec 25, 2024 00:00:00").getTime();
+
+const actualizarContador = setInterval(function() {
+
+    const ahora = new Date().getTime();
+
+    const distancia = fechaObjetivo - ahora;
+
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+
+}, 1000);
